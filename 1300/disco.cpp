8ff4cb6f07d2a8adc837b5331496300bc04ae5eb
jay jariwala <jayjariwala10125@gmail.com>
2022-04-30 12:09:36 +0530

    init

---
+++ 1300/disco.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define for0(i,n) for(int i=0;i<n;i++)
+#define vi vector<int>
+#define pb push_back
+#define sz(x) sizeof(x)
+ll op1(ll a, ll b, ll m) { a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll op2(ll a, ll b, ll m) { a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll op3(ll a, ll b, ll m) { a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+int main(){
+    int a;
+    
+     return 0;
+}
